8a779dae426c80c1c557869c2abef8de49473adb
brett <brett.beattie99@gmail.com>
2021-10-22 16:51:22 -0700

    explore done

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,38 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
-
+var synth = window.speechSynthesis;
 function init() {
   // TODO
+  var pressTalk = document.querySelector('button');
+  var selectVoice = document.querySelector('select');
+  var text = document.querySelector('textarea');
+  var utterText = new SpeechSynthesisUtterance();
+  var smiley = document.querySelector("img[src='assets/images/smiling.png']");
+
+  setTimeout(() => {
+    var voices = synth.getVoices();
+    for(var i = 0; i < voices.length; i++) {
+      var elem = document.createElement("option");
+      elem.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+      elem.value = i;
+      selectVoice.appendChild(elem);
+    }
+    selectVoice.addEventListener('change', event => {
+      utterText.voice = voices[selectVoice.value];
+    });
+  }, 50);
+
+  text.addEventListener('input', event => {
+    utterText.text = text.value;
+  });
+
+  pressTalk.addEventListener('click', event => {
+    synth.speak(utterText);
+  });
+  utterText.onstart = function(event) {
+    smiley.setAttribute('src', 'assets/images/smiling-open.png');
+  }
+  utterText.onend = function(event) {
+    smiley.setAttribute('src', 'assets/images/smiling.png');
+  }
 }
